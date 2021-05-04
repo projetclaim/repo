0205c66ba16b974957bee42729287eaa76b86a6e
Flo63 <80459799+Flo63@users.noreply.github.com>
2021-05-04 14:22:35 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -12,10 +12,19 @@ features = st.beta_container()
 modelTraining = st.beta_container()
 
 with header:
-    st.title('Ca y est ça marche aussi sous GitHub!')
+    st.title('Projet Claim Generali')
     
 st.markdown('Texte:')
 st.markdown('Texte2 ')
 
-    
+st.sidebar.title('Setting')
+st.sidebar.header('x settings')
+x_distribution = st.sidebar.selectbox(label='Choose X distribution', options=['uniform', 'normal', 'beta'])
+nb_of_lines = st.sidebar.slider(label='Number of observations', min_value=100, max_value=10000)
+st.sidebar.header('y settings')
+sigma = st.sidebar.slider(label='sigma', min_value=0., max_value=2., value=.5)
+beta1 = st.sidebar.slider(label='beta1', min_value=-3., max_value=3., value=.0)
+beta2 = st.sidebar.slider(label='beta2', min_value=-3., max_value=3., value=.0)
+beta3 = st.sidebar.slider(label='beta3', min_value=-3., max_value=3., value=.0)
+alpha = st.sidebar.slider(label='alpha', min_value=-3., max_value=3., value=.0)    
 
