054ef7c00cba2d870f8d374268a1983cae2ad3d7
Flo63 <80459799+Flo63@users.noreply.github.com>
2021-05-04 15:43:57 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -15,7 +15,7 @@ features = st.beta_container()
 modelTraining = st.beta_container()
 
 st.title('Projet Claim Generali')
-st.image('generalilogo.png')
+st.image('https://github.com/projetclaim/repo/blob/main/generalilogo.png')
 
 st.markdown('Texte1:')
 st.markdown('Texte2 ')
